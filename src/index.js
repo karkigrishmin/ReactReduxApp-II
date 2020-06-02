af1db669af2176f7346c41fe26570585535db516
Grishmin karki <grishmink@gmail.com>
2020-06-02 12:41:02 +0545

    main file

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import App from "./components/App";
+import reducers from "./reducers";
+
+//wiring up our reducers and middleware in our redux store
+const store = createStore(reducers, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
